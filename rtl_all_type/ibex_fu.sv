fd8f7c3afa97c7a5f95bd8f532b61eede4a8f6ff
LeaTEXIER <151048554+LeaTEXIER@users.noreply.github.com>
2026-03-25 10:59:59 +0100

    RTL file de Léa

---
+++ rtl_all_type/ibex_fu.sv
@@ -0,0 +1,46 @@
+module ibex_fu
+(
+ input ibex_pkg::fu_op_e    operator_i,
+ input  logic [31:0]        operand_a_i,
+ input  logic [31:0]        operand_b_i,
+ output logic [31:0]        result_o,
+ output logic               valid_o
+);
+    import ibex_pkg::*;
+    logic [4:0] shamt;
+    logic [31:5] unused_bits = operand_b_i[31:5];
+
+    always_comb begin
+        result_o  	= '0;
+        valid_o   	= 1'b0;
+        
+        shamt		= operand_b_i[4:0];
+
+        unique case (operator_i)
+            FU_MOD: begin
+                result_o = operand_a_i % operand_b_i; //32'h12345678;//operand_a_i % operand_b_i; //MODIF
+                valid_o   = 1'b1;
+            end
+            FU_MROLI: begin
+                result_o = (operand_a_i >> shamt) | (operand_a_i << (32 - shamt));
+                valid_o   = 1'b1;
+            end
+            FU_MDROLI: begin
+                result_o = (operand_b_i >> 4) | (operand_b_i << (28));
+                valid_o   = 1'b1;
+            end 
+            FU_JUMB1: begin
+                result_o = (operand_a_i ~^ operand_b_i); //NXOR
+                valid_o   = 1'b1;
+            end
+            FU_JUMB2: begin
+                result_o = operand_a_i + operand_b_i;
+                valid_o   = 1'b1;
+            end
+            default: begin
+            	result_o = 32'h20030717;
+                valid_o   = 1'b0;
+            end
+        endcase
+    end
+endmodule
